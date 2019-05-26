066294077ba3bf46deb039113bfd883579115c3a
Matt Williams <matt.seongjun@gmail.com>
2019-05-26 18:45:50 -0400

    Add assign_aquares function and begin work on building the board

--- bin/run.rb
+++ bin/run.rb
@@ -10,4 +10,36 @@ def get_names
     get_data.map{ |restaurant| restaurant["name"] }
 end
 
-puts get_names
+def get_id
+    get_data.map{ |restaurant| restaurant["id"] }
+end
+
+def build_empty_board
+    div0 = "---|---|---|---|---"
+    row1 = "   |   |   |   |   "
+    row2 = "   |   |   |   |   "
+    row3 = "   |   |   |   |   "
+    row4 = "   |   |   |   |   "
+    row5 = "   |   |   |   |   "
+end
+
+def assign_squares
+    k = 0
+    restaurants = get_id
+    squares = [[], [], [], [], []]
+    
+    for i in 0..4 do
+        for j in 0..4 do
+            if i == 2 && j == 2
+                squares[i][j] = "free"     
+            else 
+                squares[i][j] = restaurants[k]        
+                k += 1  
+            end
+        end
+    end
+
+    squares
+end
+
+p assign_squares
