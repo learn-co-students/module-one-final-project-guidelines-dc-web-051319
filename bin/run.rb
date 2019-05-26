f115a9b722950e9b46dff5ed433ee1d1eab4c24f
Matt Williams <matt.seongjun@gmail.com>
2019-05-26 17:59:05 -0400

    Add snippet of Yelp's Fusion API example and parse basic info

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,13 @@
 require_relative '../config/environment'
 
+def get_data(term="dinner", loc="Washington, DC")
+    yelp = YelpAPI.new
+    data = yelp.search(term, loc)
+    data["businesses"]
+end
 
+def get_names
+    get_data.map{ |restaurant| restaurant["name"] }
+end
 
-puts "HELLO WORLD"
+puts get_names
---
+++ lib/yelp_api.rb
@@ -0,0 +1,87 @@
+require "json"
+require "http"
+
+class YelpAPI
+
+# Place holders for Yelp Fusion's API key. Grab it
+# from https://www.yelp.com/developers/v3/manage_app
+key = ""
+File.open("/home/matt/Learn/projects/yelp_api_key.txt", 'r') do |f|
+  f.each_line do |line|
+    key = line
+  end
+end
+
+API_KEY = key
+
+
+# Constants, do not change these
+API_HOST = "https://api.yelp.com"
+SEARCH_PATH = "/v3/businesses/search"
+BUSINESS_PATH = "/v3/businesses/"  # trailing / because we append the business id to the path
+
+SEARCH_LIMIT = 24
+
+
+# Make a request to the Fusion search endpoint. Full documentation is online at:
+# https://www.yelp.com/developers/documentation/v3/business_search
+#
+# term - search term used to find businesses
+# location - what geographic location the search should happen
+#
+# Examples
+#
+#   search("burrito", "san francisco")
+#   # => {
+#          "total": 1000000,
+#          "businesses": [
+#            "name": "El Farolito"
+#            ...
+#          ]
+#        }
+#
+#   search("sea food", "Seattle")
+#   # => {
+#          "total": 1432,
+#          "businesses": [
+#            "name": "Taylor Shellfish Farms"
+#            ...
+#          ]
+#        }
+#
+# Returns a parsed json object of the request
+def search(term, location)
+  url = "#{API_HOST}#{SEARCH_PATH}"
+  params = {
+    term: term,
+    location: location,
+    limit: SEARCH_LIMIT
+  }
+
+  response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
+  response.parse
+end
+
+
+# Look up a business by a given business id. Full documentation is online at:
+# https://www.yelp.com/developers/documentation/v3/business
+#
+# business_id - a string business id
+#
+# Examples
+#
+#   business("yelp-san-francisco")
+#   # => {
+#          "name": "Yelp",
+#          "id": "yelp-san-francisco"
+#          ...
+#        }
+#
+# Returns a parsed json object of the request
+def business(business_id)
+  url = "#{API_HOST}#{BUSINESS_PATH}#{business_id}"
+
+  response = HTTP.auth("Bearer #{API_KEY}").get(url)
+  response.parse
+end
+end
