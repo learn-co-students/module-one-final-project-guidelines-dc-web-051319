acfbe313b3f3106d878d6c2c7c2fbbe3563141fe
Matt Williams <matt.seongjun@gmail.com>
2019-05-28 13:38:31 -0400

    Add fill_board method

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,36 @@
 class Board < ActiveRecord::Base
     has_many :squares
+
+    def fill_board
+        n = 1
+        k = 1
+        for i in 0..4 do
+            for j in 0..4 do
+                if i == 2 && j == 2
+                    "free"
+                else
+                    Square.new(placement: n, restaurant_id: k, board_id: self.id).save
+                    k += 1
+                end
+                n += 1
+            end
+        end
+    end
+
+    def print_board
+        board = <<-END
+        |----|----|----|----|----|
+        | 01 | 02 | 03 | 04 | 05 |
+        |----|----|----|----|----|
+        | 06 | 07 | 08 | 09 | 10 |
+        |----|----|----|----|----|
+        | 11 | 12 | fr | 14 | 15 |
+        |----|----|----|----|----|
+        | 16 | 17 | 18 | 19 | 20 |
+        |----|----|----|----|----|
+        | 21 | 22 | 23 | 24 | 25 |
+        |----|----|----|----|----|
+        END
+        board
+    end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -6,51 +6,6 @@ def get_data(term="dinner", loc="Washington, DC")
     data["businesses"]
 end
 
-def get_names
-    get_data.map{ |restaurant| restaurant["name"] }
-end
-
-def get_id
-    get_data.map{ |restaurant| restaurant["id"] }
-end
-
-def print_board
-    board = <<-END 
-    |----|----|----|----|----|
-    | 01 | 02 | 03 | 04 | 05 |
-    |----|----|----|----|----|
-    | 06 | 07 | 08 | 09 | 10 |
-    |----|----|----|----|----|
-    | 11 | 12 | fr | 14 | 15 |
-    |----|----|----|----|----|
-    | 16 | 17 | 18 | 19 | 20 |
-    |----|----|----|----|----|
-    | 21 | 22 | 23 | 24 | 25 |
-    |----|----|----|----|----|
-    END
-    board
-end
-
-def assign_squares
-    n = 1
-    k = 0
-    restaurants = get_id
-    squares = [[], [], [], [], []]
-    
-    for i in 0..4 do
-        for j in 0..4 do
-            if i == 2 && j == 2
-                squares[i][j] = { index: "free", yelp_id: nil }     
-            else 
-                squares[i][j] = { index: n, yelp_id: restaurants[k]}         
-                k += 1  
-            end
-            n += 1
-        end
-    end
-    squares
-end
-
 def make_users
     matt = User.new
     tim = User.new
@@ -73,6 +28,7 @@ def make_boards
     b1 = Board.new
     b1.date = "April 2017"
     b1.save
+    b1.fill_board
 end
 
 
@@ -83,4 +39,5 @@ matt = User.find_by_name("Matt")
 restaurant = Restaurant.find(1)
 matt.have_meal(restaurant)
 puts Visit.all
+puts Square.all
 binding.pry
