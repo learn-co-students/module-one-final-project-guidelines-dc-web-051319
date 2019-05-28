cd3faea9d502e21f34957e654b691383a23e426a
villatrue <scarter0313@gmail.com>
2019-05-28 15:49:37 -0400

    Update 02

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,45 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_monster_cards_from_api(x)
-    response_string = RestClient.get('https://db.ygoprodeck.com/api/v4/cardinfo.php')
-    response_hash = JSON.parse(response_string)
-    
-    new_array = []
-    cards = response_hash[0].select do |hash|
-        hash["type"] == "Normal Monster"
-        response_string = RestClient.get(hash)
-        hash_info = JSON.parse(response_string)
-        new_array << hash_info
-    end
-    new_array
-        
-    end
-    binding.pry
-    0
+
+all_cards = nil
+
+def all_cards
+  response_string = RestClient.get('https://db.ygoprodeck.com/api/v4/cardinfo.php')
+  response_hash = JSON.parse(response_string)
 end
+
+
+def get_monster_cards_from_api
+  monster_cards = all_cards[0].select do |hash|
+    hash["type"] == "Normal Monster"
+    end
+  puts "How Many Monster Cards Would You Like to draw"
+  x = gets.chomp.to_i
+  monster_cards.sample(x)
+end
+
+def get_trap_cards_from_api
+  trap_cards = all_cards[0].select do |hash|
+    hash["type"] == "Trap Card"
+    end
+  puts "How Many Trap Cards Would You Like to draw"
+  x = gets.chomp.to_i
+  trap_cards.sample(x)
+end
+
+def get_specific_card
+  puts "What specific card would you like(case sensitive)"
+  card_name = gets.chomp
+selected_card =  all_cards[0].find do |hash|
+    if hash["name"] == card_name
+      return hash
+    end
+end
+end
+
+
+binding.pry
+
+
+    0
