68cab371625d9db77f2c6171788d1e8c0d29245c
Matt Williams <matt.seongjun@gmail.com>
2019-05-29 10:53:38 -0400

    Remove console output of marked_square array

--- app/models/board.rb
+++ app/models/board.rb
@@ -17,48 +17,31 @@ class Board < ActiveRecord::Base
 
 
     def print_board(user)
-        ref = <<-END
-        |----|----|----|----|----|
-        | 01 | 02 | 03 | 04 | 05 |
-        |----|----|----|----|----|
-        | 06 | 07 | 08 | 09 | 10 |
-        |----|----|----|----|----|
-        | 11 | 12 | fr | 14 | 15 |
-        |----|----|----|----|----|
-        | 16 | 17 | 18 | 19 | 20 |
-        |----|----|----|----|----|
-        | 21 | 22 | 23 | 24 | 25 |
-        |----|----|----|----|----|
-        END
-
-    
 
         board = [  ["   ","   ","   ","   ","   "], ["   ","   ","   ","   ","   "], ["   ","   ", "\u2B50  ","   ","   "], ["   ","   ","   ","   ","   "], ["   ","   ","   ","   ","   "]  ]
 
         # y = "\u2B50"
-     
+
         # puts y.encode('utf-8')
-       
+
 
         visits = user.visits
-        restaurant_ids = visits.map{ |visit| visit.restaurant_id } 
+        restaurant_ids = visits.map{ |visit| visit.restaurant_id }
         marked_squares = restaurant_ids.map{ |id| Square.find_by(restaurant_id: id) }
-
-        p marked_squares
 
         # cross = "\u274C"
         # puts cross.encode('utf-8')
-        
+
         marked_squares.each do |square|
-            row = square.row 
+            row = square.row
             column = square.column
             board[row][column] = " \u274C "
         end
 
-        
-        
+
+
         div = "-\u2501-\u254B-\u2501-\u254B-\u2501-\u254B-\u2501-\u254B-\u2501-"
-        
+
         puts board[0].join("\u2503")
         puts div
         puts board[1].join("\u2503")
@@ -71,4 +54,3 @@ class Board < ActiveRecord::Base
     end
 
 end
-
