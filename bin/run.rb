e16576f2965b83315521436a6641f5d2c9d13fc3
tariqjunaid <junaidtariq_91@yahoo.com>
2019-06-01 14:38:34 -0400

    Update

--- bin/run.rb
+++ bin/run.rb
@@ -27,44 +27,12 @@ time_to_build
 
 here_deck(player)
 
-<<<<<<< HEAD
 puts "\n"
 
 card_to_add = add_card_prompt
 add = add_single_card(card_to_add, player)
 
 puts "\n"
-=======
-
-
-def add_single_card(card_name, player)
- here = player.decks.first.cards
-  card = Card.find_by(name: card_name)
-  if card
-    here << card
-    puts "You have added #{card.name}"
-    # binding.pry
-  else puts "card doesnt exist"
-  end
-end
-
-add_single_card(card_name, player)
-
-def remove_card(card_name, player)
-  here = player.decks.first.cards
-   card = here.find_by(name: card_name)
-   if card
-     here.delete(card)
-     puts "You have deleted #{card.name}"
-   else puts "Either you don't want to remove a card or it doesn't exist in your deck"
-   end
-end
-
-def remove_card_prompt
- puts  "Time to remove a card"
-  gets.chomp
-end
->>>>>>> cd6eed2397647c20d135554228ed33613110ce27
 
 card_to_delete = remove_card_prompt
 remove_card(card_to_delete, player)
@@ -82,7 +50,7 @@ def find_strongest_attack_card(player)
   # binding.pry
 end
 
-end
+
 
 find_strongest_card_prompt
 
